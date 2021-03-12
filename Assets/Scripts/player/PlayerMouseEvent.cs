bab6ee76a1d3052150dd7b771829be188c4b5ebd
5tarlight <developerstarlight@gmail.com>
2021-03-13 00:12:42 +0900

    crude player moving

---
+++ Assets/Scripts/player/PlayerMouseEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minecraft.Player
+{
+  public class PlayerMouseEvent : MonoBehaviour
+  {
+  }  
+}
---
+++ Assets/Scripts/player/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minecraft.Player
+{
+  public class PlayerMove : MonoBehaviour
+  {
+    private Rigidbody rigid;
+    private int movingMultiplyer;
+
+    private void Awake()
+    {
+      rigid = GetComponent<Rigidbody>();
+      movingMultiplyer = 10;
+    }
+
+    private void FixedUpdate()
+    {
+      var hor = Input.GetAxis("Horizontal") * movingMultiplyer;
+      var ver = Input.GetAxis("Vertical") * movingMultiplyer;
+      
+      rigid.AddForce(new Vector3(hor, 0, ver));
+
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        rigid.AddForce(new Vector3(0, 350, 0));
+      }
+    }
+  }  
+}
